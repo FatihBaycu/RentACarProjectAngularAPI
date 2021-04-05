d486e9d5419043fd1424204a8e22f684a9661691
FatihBaycu <email@example.com>
2021-04-06 00:04:00 +0300

    .

--- src/app/components/car-add/car-add.component.ts
+++ src/app/components/car-add/car-add.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
+import { Brand } from 'src/app/models/brand/brand';
 import { Car } from 'src/app/models/car/car';
+import { Color } from 'src/app/models/color/color';
+import { BrandService } from 'src/app/services/brand/brand.service';
 import { CarService } from 'src/app/services/car/car.service';
+import { ColorService } from 'src/app/services/color/color.service';
 
 @Component({
   selector: 'app-car-add',
@@ -13,22 +17,37 @@ export class CarAddComponent implements OnInit {
 
   carAddForm:FormGroup;
   car:Car[];
+  colors: Color[];
+  brands: Brand[];
   
   constructor(
               private carService:CarService,
               private toastrService:ToastrService,
-              private formBuilder:FormBuilder             
+              private formBuilder:FormBuilder,
+              private colorService: ColorService,
+              private brandService: BrandService        
               ) { }
 
   ngOnInit(): void {
+    this.getBrands();
+    this.getColors();
     this.createAddCarForm();
   }
 
-
+  getColors() {
+    this.colorService.getColors().subscribe((response) => {
+      this.colors = response.data;
+    });
+  }
+  getBrands() {
+    this.brandService.getBrands().subscribe((response) => {
+      this.brands = response.data;
+    });
+  }
     createAddCarForm(){
         this.carAddForm=this.formBuilder.group({
-        brandId:["",Validators.required],
-        colorId:["",Validators.required],
+        brandId:[1,Validators.required],
+        colorId:[1,Validators.required],
         modelYear:["",Validators.required],
         dailyPrice:["",Validators.required],
         description:["",Validators.required],
